c9de7e50f0a160f6129097e64dc8e4ac15a3b997
Gustavo Cavalcante <guscavalcante@192.168.1.3>
2022-07-08 11:29:51 -0300

    Adding navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app! 2</Text>
+    </View>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
