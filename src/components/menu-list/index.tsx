5b0e29adc246b0d84bc8126afed17c65accb6d6f
Gustavo Cavalcante <guscavalcante@192.168.1.3>
2022-07-09 09:29:10 -0300

    Adjusting

--- src/components/menu-list/index.tsx
+++ src/components/menu-list/index.tsx
@@ -1,10 +1,7 @@
-import { View, Text, ScrollView } from 'react-native';
+import { View, Text, ScrollView, FlatList } from 'react-native';
 import MenuItem from '../menu-item';
 import { styles } from './styles';
 import sizes from '../../styles/sizes';
-import { Item } from '../../types/item';
-
-// const items = ['xxx', 'yyy', 'zzz', 'xxxx'];
 
 const MenuList = ({items, title}): JSX.Element => {
 
@@ -13,11 +10,7 @@ const MenuList = ({items, title}): JSX.Element => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{title}</Text>
-      <ScrollView contentContainerStyle={{paddingLeft: paddingSize}} horizontal>
-        {
-          items.map((i: Item, index) => <MenuItem item={i} paddingSize={paddingSize}/>)
-        }
-      </ScrollView>
+      <FlatList style={{paddingLeft: paddingSize}} horizontal renderItem={({item}) => <MenuItem item={item} paddingSize={paddingSize}/>} data={items}/>
     </View>
   );
 };
